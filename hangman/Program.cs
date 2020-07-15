bfc09224511a190f90abd7937dca5f9ddc863b54
Ficu <jakubficek@outlook.com>
2020-07-15 18:43:10 +0200

    tt

--- hangman/Program.cs
+++ hangman/Program.cs
@@ -6,7 +6,7 @@ namespace hangman
     {
         public static void Main(string[] args)
         { 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("World!");
         }
     }
 }
